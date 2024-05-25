a1223e24dc4ece56ad20aefc0718965fbe208cc2
Aruzhan Ismagulova <aruzhanism@mail.ru>
2024-05-26 00:37:42 +0500

    configured playfab service and google play service api

--- Assets/Scripts/UserAccountManager.cs
+++ Assets/Scripts/UserAccountManager.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
 using UnityEngine;
 
 public class UserAccountManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private TextMeshProUGUI detailsText; 
     void Start()
     {
-        
+        PlayGamesPlatform.Activate();
+        PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
     }
 
-    // Update is called once per frame
-    void Update()
+    internal void ProcessAuthentication(SignInStatus status)
     {
-        
+        if (status == SignInStatus.Success)
+        {
+            Debug.Log("Authentication successful.");
+            detailsText.text = "Authentication successful.";
+            PlayGamesPlatform.Instance.RequestServerSideAccess(false, ProcessServerAuthCode);
+        }
+        else
+        {
+            detailsText.text = "Authentication failed.";
+            Debug.LogError("Authentication failed: " + status); 
+        }
+    }
+
+    private void ProcessServerAuthCode(string serverAuthCode)
+    {
+        Debug.Log("Server Auth Code: " + serverAuthCode);
+
+        var request = new LoginWithGooglePlayGamesServicesRequest
+        {
+            ServerAuthCode = serverAuthCode,
+            CreateAccount = true,
+            TitleId = PlayFabSettings.TitleId
+        };
+
+        PlayFabClientAPI.LoginWithGooglePlayGamesServices(request, OnLoginWithGooglePlayGamesServicesSuccess, OnLoginWithGooglePlayGamesServicesFailure);
+    }
+
+    private void OnLoginWithGooglePlayGamesServicesSuccess(LoginResult result)
+    {
+        Debug.Log("PF Login Success LoginWithGooglePlayGamesServices");
+        detailsText.text = "success";
+    }
+
+    private void OnLoginWithGooglePlayGamesServicesFailure(PlayFabError error)
+    {
+        Debug.Log("PF Login Failure LoginWithGooglePlayGamesServices: " + error.GenerateErrorReport());
+        detailsText.text = "fail";
     }
 }
